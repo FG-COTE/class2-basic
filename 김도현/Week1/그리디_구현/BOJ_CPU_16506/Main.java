e00005593326950f5cf07a003166ac7717acb93f
K-Diger <tigerkingdh991009@gmail.com>
2022-09-19 16:25:48 +0900

    220919 CPU (피드백 주석 메모)

--- 김도현/Week1/그리디_구현/BOJ_CPU_16506/Main.java
+++ 김도현/Week1/그리디_구현/BOJ_CPU_16506/Main.java
@@ -100,17 +100,19 @@ public class Main {
             String rBByCommand = command[3];
 
 
-            // op 코드 해석
+            // op 코드 매핑 -> 정답 문자열에 삽입
             for (int j = 0; j < 5; j++) {
                 machineCode.append(opcodeByCommand[j]);
             }
 
+            // 사용하지 않는 레지스터 정답 문자열에 삽입
             machineCode.append("0");
 
             //System.out.println(Integer.parseInt(rDByCommand));
             //System.out.println(Integer.parseInt(rAByCommand));
             //System.out.println(Integer.parseInt(rBByCommand));
 
+            // 각 레지스터 Binary 화
             String binaryRD = Integer.toBinaryString(Integer.parseInt(rDByCommand));
             String binaryRA = Integer.toBinaryString(Integer.parseInt(rAByCommand));
             String binaryRB = Integer.toBinaryString(Integer.parseInt(rBByCommand));
@@ -119,22 +121,34 @@ public class Main {
             //System.out.println("binaryRA = " + binaryRA);
             //System.out.println("binaryRB = " + binaryRB);
 
+            // Binary 변환 레지스터 크기보다 작으면, 자릿수 채워주기
             if (binaryRD.length() < rD) {
                 binaryRD = "0".repeat(rD - binaryRD.length()) + binaryRD;
             }
 
+            // Binary 변환 레지스터 크기보다 작으면, 자릿수 채워주기
             if (binaryRA.length() < rA) {
                 binaryRA = "0".repeat(rA - binaryRA.length()) + binaryRA;
             }
 
-            int bRBLength = binaryRB.length();
+            // Binary 변환 레지스터 크기보다 작으면, 자릿수 채워주기
+            // RB 는 변환 후 추가 작업이 필요하다.
+            // 1. 만약 변환한 rB 길이가 rB 레지스터보다 작고, OPCODE 마지막 문자가 C 로 끝나는 게 아니면
+            // 1. 맨 마지막 비트는 0으로 고정된다.
+
+            int rBLength = binaryRB.length();
             int opcodeLength = command[0].length() - 1;
 
-            if (bRBLength < rB && command[0].charAt(opcodeLength) == 'C') {
-                binaryRB = "0".repeat(rB - binaryRB.length()) + binaryRB;
+            // 궁금증. 조건문마다 다르게 해줘야 되던데 왜그럴까.
+            //  binaryRB = "0".repeat(rB - rBLength) + binaryRB;
+            //  binaryRB = "0".repeat(rB - rBLength - 1) + binaryRB;
+            if (rBLength < rB && command[0].charAt(opcodeLength) == 'C') {
+                System.out.println("rB - rBLength = " + (rB - rBLength));
+                binaryRB = "0".repeat(rB - rBLength) + binaryRB;
             }
-            else if (bRBLength < rB && command[0].charAt(opcodeLength) != 'C') {
-                binaryRB = "0".repeat(rB - binaryRB.length() - 1) + binaryRB + "0";
+            else if (rBLength < rB && command[0].charAt(opcodeLength) != 'C') {
+                System.out.println("rB - rBLength - 1 = " + (rB - rBLength - 1));
+                binaryRB = "0".repeat(rB - rBLength - 1) + binaryRB + "0";
             }
 
             machineCode.append(binaryRD);
