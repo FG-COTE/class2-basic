29d7e6bb89188ac4735f5dc4828f0b7b116e9191
Eul Noh <81086966+0sunset0@users.noreply.github.com>
2022-09-20 19:56:03 +0900

    week1

--- 노을/1주차/16506CPU.py
+++ 노을/1주차/16506CPU.py
@@ -1,6 +1,6 @@
 n = int(input())
 
-opcode_assembly = {'ADD':'0000', 'SUB':'0001', 'MOV':'0010', 'AND':'0011','OR':'0100',
+opcode_machine = {'ADD':'0000', 'SUB':'0001', 'MOV':'0010', 'AND':'0011','OR':'0100',
                    'NOT':'0101','MULT':'0110', 'LSFTL':'0111', 'LSFTR':'1000',
                    'ASFTR':'1001','RL':'1010','RR':'1011'}
 for i in range(n):
@@ -12,10 +12,10 @@ for i in range(n):
     rb = input_data[3]
     
     if opcode[-1]=='C':
-        machine_code += opcode_assembly.get(opcode[:-1])
+        machine_code += opcode_machine.get(opcode[:-1])
         machine_code += '1'
     else:
-        machine_code += opcode_assembly.get(opcode)
+        machine_code += opcode_machine.get(opcode)
         machine_code += '0'
     machine_code += '0'
     
@@ -29,5 +29,4 @@ for i in range(n):
         machine_code+=str(bin(int(rb)))[2:].zfill(4)
         
     print(machine_code)
-     
      
