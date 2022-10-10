b46c4c4cd1838b3fe83418e2cb4072010f664ef1
0sunset0 <chi6465618@naver.com>
2022-10-10 20:51:46 +0900

    week3

--- 노을/3주차/16173점프왕쩰리.py
+++ 노을/3주차/16173점프왕쩰리.py
@@ -8,24 +8,22 @@ result = []
 
 def dfs(num_list, x, y):
     value = num_list[x][y]
-    if value==-1:
+    if value==0:
+        return
+    elif value==-1:
         result.append(True)
         return 
     else:
         if x+value>=n and y+value>=n:
             result.append(False)
             return
-        else:
-            if x+value<n:
-                dfs(num_list, x+value, y)
-                return
-            if y+value<n:
-                dfs(num_list, x, y+value)
-                return
+        if x+value<n:
+            dfs(num_list, x+value, y)
+        if y+value<n:
+            dfs(num_list, x, y+value)
+    return
         
 
-
-            
 
 dfs(num_list, 0, 0)
 if True in result:
