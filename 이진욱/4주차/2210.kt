dc31a289ef017bf803492636009cce5640b90000
이진욱 <koownij@kakao.com>
2022-11-07 21:32:41 +0900

    Update 2210.kt

--- 이진욱/4주차/2210.kt
+++ 이진욱/4주차/2210.kt
@@ -6,7 +6,7 @@ import kotlin.math.min
 private val br = BufferedReader(InputStreamReader(System.`in`))
 val graph = mutableListOf<List<Int>>()
 val sets = mutableSetOf<String>()
-val dirs = listOf(listOf(1, 0), listOf(0, 1), listOf(-1, 0), listOf(0, -1))
+val dirs = listOf(listOf(1, 0), listOf(0, 1), listOf(-1, 0), listOf(0, -1)) // 상, 하, 좌, 우
 
 fun main(args: Array<String>) {
     repeat(5) {
@@ -17,16 +17,24 @@ fun main(args: Array<String>) {
         for(j in 0 until 5) {
             dfs(i, j, "", 0)
         }
-    
+
     print(sets.size)
 }
 
+/**
+ * prevI : 이전에 방문한 i 값
+ * prevJ : 이전에 방문한 j 값
+ * prevStr : 이때까지 방문하며 만들어진 수
+ * cnt : 방문 횟수
+ */
 fun dfs(prevI: Int, prevJ: Int, prevStr: String, cnt: Int) {
+    // 방문 횟수가 6인 경우, 이때까지 방문하며 만들어진 수를 set에 넣는다.
     if(cnt == 6) {
         sets.add(prevStr)
         return
     }
 
+    // 상하좌우로 이동하며 dfs 수행
     for((di,dj) in dirs) {
         val newI = prevI + di
         val newJ = prevJ + dj
@@ -35,3 +43,16 @@ fun dfs(prevI: Int, prevJ: Int, prevStr: String, cnt: Int) {
         dfs(newI, newJ, prevStr + "${graph[newI][newJ]}", cnt + 1)
     }
 }
+
+/**
+ * 첫번째로 밝을 수 있는 숫자판의 개수 : 25
+ * 두번째로 밟을 수 있는 숫자판의 개수 : 4 (상, 하, 좌, 우)
+ * 세번재로 밟을 수 있는 숫자판의 개수 : 4 (상, 하, 좌, 우)
+ * 네번째로 밟을 수 있는 숫자판의 개수 : 4 (상, 하, 좌, 우)
+ * 다섯번째로 밟을 수 있는 숫자판의 개수 : 4 (상, 하, 좌, 우)
+ * 여섯번째로 밟을 수 있는 숫자판의 개수 : 4 (상, 하, 좌, 우)
+ * 
+ * 최악의 경우의 수 : 25 * 4 * 4 * 4 * 4 * 4 = 25,600
+ * 
+ * dfs + 브루트포스(완전탐색) 하면 된다.
+ */
