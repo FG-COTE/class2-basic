e5ce418c90a4623a67f2aadbfc1a1add977b774b
Abs0c <91037835+Abs0c@users.noreply.github.com>
2022-11-14 12:47:14 +0900

    Create baek_9655.java

---
+++ 김동욱/5주차/baek_9655.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class baek_9655 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[1001];
+        arr[1] = 1;//SK
+        arr[2] = -1;//CY
+        arr[3] = 1;//SK
+        if (dp(arr, n) == 1){
+            System.out.println("SK");
+        }
+        else{
+            System.out.println("CY");
+        }
+    }
+
+    public static int dp(int[] arr, int n){
+        if (arr[n] == 0){
+            if (n > 3){
+                arr[n] = dp(arr, n-3) * -1;
+            }
+            else{
+                arr[n] = dp(arr, n-1) * -1;
+            }
+        }
+        return arr[n];
+    }
+}
