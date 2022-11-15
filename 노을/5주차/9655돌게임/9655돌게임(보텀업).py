1c49e8e8f42ecd20aeac0b4f725ac8413c7b8704
0sunset0 <chi6465618@naver.com>
2022-11-15 11:17:21 +0900

    week5

--- 노을/5주차/9655돌게임/9655돌게임(보텀업).py
+++ 노을/5주차/9655돌게임/9655돌게임(보텀업).py
@@ -7,9 +7,6 @@ if n!=1:
 
 if n>2:
     for i in range(3, n+1):
-        if dp[i-1]=='SK':
-            dp[i] = 'CY'
-        else: 
-            dp[i] = 'SK'
+        dp[i] = dp[i-2]
             
 print(dp[n])
