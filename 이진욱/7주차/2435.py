a084474c957227b0de78f972c8a6d86929ce6e4e
이진욱 <koownij@kakao.com>
2022-12-19 12:45:18 +0900

    Update 2435.py

--- 이진욱/7주차/2435.py
+++ 이진욱/7주차/2435.py
@@ -6,8 +6,8 @@ for li in lists:
     s += li
     sums.append(s)
 
-answer = sums[k] - sums[0]
-for i in range(1, n - k + 1):
+answer = sums[k] - sums[0] # 0 ~ k번째까지의 합
+for i in range(1, n - k + 1): # 1 ~ k + 1번째까지 합, 2 ~ k + 2, 3 ~ k + 3, ... 까지의 합을 구하고 그 중 가장 큰 값을 출력한다.
     if answer < sums[k + i] - sums[i]:
         answer = sums[k + i] - sums[i]
 
