708d8c162eb7f4edc4c621e4c8468c5f884afce6
Abs0c <91037835+Abs0c@users.noreply.github.com>
2023-01-03 17:36:38 +0900

    Update 4949.java

--- 김동욱/8주차/4949.java
+++ 김동욱/8주차/4949.java
@@ -13,11 +13,14 @@ public class Main {
             }
             Stack<String> stack = new Stack<>();
             for (int i = 0; i < string.length() - 1; i++){
+                //( 와 [ 같이 괄호를 여는 경우는 일단 스택에 넣고
                 if (string.charAt(i) == "(".charAt(0) || string.charAt(i) == "[".charAt(0)){
                     stack.push(Character.toString(string.charAt(i)));
                 }
+                //만약에 ) 면 스택의 맨 윗값이 (가 아니거나 스택이 비어있으면 문제의 조건에 따라
+                //문자열의 균형이 이뤄지지 않은 상태이므로..
                 else if (string.charAt(i) == ")".charAt(0)){
-                    if (!stack.isEmpty() && stack.peek().equals("(")){
+                    if (!stack.isEmpty() && stack.peek().equals("(")){ 
                         stack.pop();
                     }
                     else{
@@ -25,6 +28,8 @@ public class Main {
                         break;
                     }
                 }
+                //만약에 ] 면 스택의 맨 윗값이 [가 아니거나 스택이 비어있으면 문제의 조건에 따라
+                //문자열의 균형이 이뤄지지 않은 상태이므로..
                 else if (string.charAt(i) == "]".charAt(0)){
                     if (!stack.isEmpty() && stack.peek().equals("[")){
                         stack.pop();
@@ -34,9 +39,11 @@ public class Main {
                         break;
                     }
                 }
+                //for문이 끝날때 스택이 비어있어야 모든 괄호가 정상적으로 닫힌것이므로
                 if (stack.isEmpty() && i == string.length() - 2){
                     System.out.println("yes");
                 }
+                //for문이 끝날때 스택이 안 비어있으면 문자열이 균형을 이루고 있지 않은것이므로
                 else if (!stack.isEmpty() && i == string.length() - 2){
                     System.out.println("no");
                 }
